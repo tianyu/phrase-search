3d100aeb4a5f5c7556a10a53a029dc8bb87815cc
Tianyu Zhu <tianyu.zhu@tamr.com>
2014-10-13 19:17:12 -0400

    More details for Corpus.find

--- src/main/java/Corpus.java
+++ src/main/java/Corpus.java
@@ -2,6 +2,7 @@ public interface Corpus {
 
 	/**
 	 * Gets all of the positions where the given token appears.
+	 * <p>The search is case-insensitive and all whitespace is trimmed.</p>
 	 *
 	 * @param token The token to look for.
 	 * @return A {@link Cursor} that iterates over all {@link DPT}s
