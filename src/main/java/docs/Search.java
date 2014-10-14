2530fcf2d9c795b1d45184c4b8193af4ea73463b
Tianyu Zhu <tianyu.zhu@tamr.com>
2014-10-14 11:35:29 -0400

    Added double word search

--- src/main/java/docs/Search.java
+++ src/main/java/docs/Search.java
@@ -17,4 +17,14 @@ public class Search {
 	public List<Integer> findWord(String word) {
 		return null;
 	}
+
+	/**
+	 * Find all documents in the corpus containing both words.
+	 * @param word1 The first word to search for.
+	 * @param word2 The second word to search for.
+	 * @return A list of all document numbers containing both words.
+	 */
+	public List<Integer> findTwoWords(String word1, String word2) {
+		return null;
+	}
 }
---
+++ src/test/java/docs/DoubleWordSearchTest.java
@@ -0,0 +1,19 @@
+package docs;
+
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+public class DoubleWordSearchTest {
+	private final Search search = new Search(TestCorpus.load("people"));
+
+	public void testFindTwoWords() {
+		final List<Integer> jimmiClay = search.findTwoWords("jimmi", "clay");
+		assertThat(jimmiClay, contains(4));
+
+		final List<Integer> isA = search.findTwoWords("is", "a");
+		assertThat(isA, containsInAnyOrder(0, 1, 3, 4, 5, 6, 7, 8));
+	}
+}
